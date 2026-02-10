b44ab48e65b83b20e8010fb4d8151da4da194020
Captain ALM <captainalm@captainalm.com>
2026-02-10 12:38:06 +0000

    Add jti IAT support. Clean deps.

--- database_token/queries/subjectIAT.sql
+++ database_token/queries/subjectIAT.sql
@@ -10,11 +10,32 @@ UPDATE subjectIAT SET min_iat = ? WHERE subject = ?;
 -- name: UpdateSubjectMinRefreshIAT :execresult
 UPDATE subjectIAT SET min_iat = ?, min_refresh_iat = ?  WHERE subject = ?;
 
--- name: GetMinIAT :one
+-- name: GetMinSubjectIAT :one
 SELECT min_iat FROM subjectIAT WHERE subject = ? LIMIT 1;
 
--- name: GetMinRefreshIAT :one
+-- name: GetMinRefreshSubjectIAT :one
 SELECT min_refresh_iat FROM subjectIAT WHERE subject = ? LIMIT 1;
 
--- name: GetMinIATRefreshIAT :one
+-- name: GetMinIATRefreshSubjectIAT :one
 SELECT min_iat, min_refresh_iat FROM subjectIAT WHERE subject = ? LIMIT 1;
+
+-- name: AddJTIEntry :execresult
+INSERT INTO jtiIAT (jti, min_iat, min_refresh_iat) VALUES (?, ? ,?);
+
+-- name: RemoveJTIEntry :execresult
+DELETE FROM jtiIAT WHERE jti = ?;
+
+-- name: UpdateJTIMinIAT :execresult
+UPDATE jtiIAT SET min_iat = ? WHERE jti = ?;
+
+-- name: UpdateJTIMinRefreshIAT :execresult
+UPDATE jtiIAT SET min_iat = ?, min_refresh_iat = ?  WHERE jti = ?;
+
+-- name: GetMinJTIIAT :one
+SELECT min_iat FROM jtiIAT WHERE jti = ? LIMIT 1;
+
+-- name: GetMinRefreshJTIIAT :one
+SELECT min_refresh_iat FROM jtiIAT WHERE jti = ? LIMIT 1;
+
+-- name: GetMinIATRefreshJTIIAT :one
+SELECT min_iat, min_refresh_iat FROM jtiIAT WHERE jti = ? LIMIT 1;
--- database_token/schemas/0_init.up.sql
+++ database_token/schemas/0_init.up.sql
@@ -3,3 +3,9 @@ CREATE TABLE IF NOT EXISTS subjectIAT (
     min_iat bigint NOT NULL,
     min_refresh_iat bigint NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS jtiIAT (
+    jti varchar(2048) NOT NULL PRIMARY KEY,
+    min_iat bigint NOT NULL,
+    min_refresh_iat bigint NOT NULL
+);
